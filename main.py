daae0213301102dc556c64c876cf7aff6ee81e01
Abhyudaya Gupta <abhyudayagupta123l@gmail.com>
2026-03-23 23:50:23 +0530

    add root route

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API is running 🚀"}
 
 # Allow frontend (Lovable) to call this API
 app.add_middleware(
